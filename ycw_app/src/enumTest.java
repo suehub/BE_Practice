7f5c2675b7b87ff74a11b45b225f389a6f0adab8
tunastorm <flatsticker3@gmail.com>
2024-04-23 20:26:15 +0900

    update all queries are placed in enum class

---
+++ ycw_app/src/enumTest.java
@@ -0,0 +1,14 @@
+
+import repository.Query;
+
+public class enumTest {
+    public static void main(String[] args) {
+
+        Query query = Query.USER_LOG_IN;
+
+        System.out.println(query.getQueryString());
+
+
+    }
+
+}
